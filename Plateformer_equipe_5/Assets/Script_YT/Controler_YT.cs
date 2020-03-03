36c2ce97b311a40c9f1a0de40a07c364f6440845
navoy <y.thieme@rubika-edu.com>
2020-03-03 15:58:16 +0100

    Préfab joueur

---
+++ Plateformer_equipe_5/Assets/Script_YT/Controler_YT.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controler_YT : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float xAxis;
+
+    public float moveSpeed;
+
+    public float jumpForce;
+
+    public float groundDectectionRadius;
+
+    public LayerMask whatIsGround;
+
+    private Transform groundDetectionObject;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        groundDetectionObject = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xAxis = Input.GetAxis("Horizontal");
+        doJump();
+
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, rb.velocity.y ,0f);
+    }
+
+    void doJump ()
+    {
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded())
+        {
+            rb.velocity += Vector2.up * jumpForce;
+        }
+    }
+
+    bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(groundDetectionObject.position, groundDectectionRadius, whatIsGround);
+    }
+}
