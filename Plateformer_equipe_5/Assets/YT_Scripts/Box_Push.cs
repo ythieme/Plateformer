efe5a4bfb3276b8243c63a5aa066ffb545428453
Navoy <y.thieme@rubika-edu.com>
2020-04-09 16:14:49 +0200

    Avancement mécanique de pousse

---
+++ Plateformer_equipe_5/Assets/YT_Scripts/Box_Push.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box_Push : MonoBehaviour
+{
+    public Player_Push pushingTrigered;
+    private int movement;
+    private BoxCollider2D boxCollider2d;
+    public LayerMask boxType;
+    public float gravity;
+    public float raylenght;
+    public LayerMask groundLayerMask;
+    public float overlapCircleRadius;
+    private int isGrounded;
+    //RaycastHit2D groundDectector;
+
+   
+
+    void Start()
+    {
+        boxCollider2d = GetComponent<BoxCollider2D>();
+        raylenght = 5f;
+    }
+    void Update()
+    {
+        //GrounDetector();
+    }
+
+    int BoxMovement()
+    {
+        if (pushingTrigered == true)
+        {
+
+
+
+        }
+        return movement;
+    }
+
+   /* public Physics2D GrounDetector()
+    {
+        {
+            Physics2D.OverlapCircle(new Vector2(boxCollider2d.bounds.center.x, 5f), ContactFilter2D.SetLayerMask, boxCollider2d[]) ;
+        }
+
+        return ;
+    }
+
+    /*private RaycastHit2D GroundDetector()
+    {
+        {
+            RaycastHit2D raycastHit = Physics2D.Raycast(new Vector2(boxCollider2d.bounds.max.x, boxCollider2d.bounds.min.y), Vector2.right,
+                raylenght, boxType);
+         Debug.DrawRay(new Vector2(boxCollider2d.bounds.min.x, boxCollider2d.bounds.min.y), Vector2.right * raylenght, Color.yellow);
+         groundDectector = raycastHit;
+        }
+
+        return groundDectector;
+    } */
+}
---
+++ Plateformer_equipe_5/Assets/YT_Scripts/Player_Push.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Push : MonoBehaviour
+{
+    public LayerMask boxLayermask;
+    RaycastHit2D boxDetector;
+    private BoxCollider2D boxCollider;
+    bool right;
+    public float rayLength;
+    bool isPushing;
+    bool movingObject;
+
+    private void Awake()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+    void Start()
+    {
+        rayLength = 0.3f;
+    }
+
+    void Update()
+    {
+        Movement();
+        Pushingconditions();
+    }
+
+
+    bool Movement()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            right = true;
+        }
+
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            right = false;
+        }
+
+        return right;
+    }
+
+    public RaycastHit2D BoxDetector()
+    {
+
+        if (right == true)
+        {
+            RaycastHit2D raycastHit = Physics2D.Raycast(new Vector2(boxCollider.bounds.center.x, boxCollider.bounds.center.y), Vector2.right,
+                rayLength, boxLayermask); 
+            Debug.DrawRay(new Vector2(boxCollider.bounds.center.x, boxCollider.bounds.center.y), Vector2.right*rayLength, Color.magenta);
+            boxDetector = raycastHit;
+        }
+
+        else if (right == false)
+        {
+            RaycastHit2D raycastHit = Physics2D.Raycast(new Vector2(boxCollider.bounds.center.x, boxCollider.bounds.center.y),
+                Vector2.left, rayLength, boxLayermask); 
+            Debug.DrawRay(new Vector2(boxCollider.bounds.center.x, boxCollider.bounds.center.y), Vector2.left*rayLength, Color.magenta);
+            boxDetector = raycastHit;
+        }
+
+        return boxDetector;
+    }
+
+    bool Pushingconditions()
+    {
+        if (BoxDetector() && Input.GetKey(KeyCode.E))
+        {
+            isPushing = true;
+        }
+        else
+        {
+            isPushing = false;
+        }
+        return isPushing;
+    }
+
+    bool PushingConditionValidated()
+    {
+        if (isPushing == true)
+        {
+
+
+
+        }
+
+        return movingObject;
+    }
+
+
+}
