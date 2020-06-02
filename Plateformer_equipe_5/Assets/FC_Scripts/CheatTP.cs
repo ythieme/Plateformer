004f3858eba287804b5808ba46d0a0261f0cfefc
Félix <f.courtemanche@rubika-edu.com>
2020-06-02 11:49:12 +0200

    Préparatifs TP

---
+++ Plateformer_equipe_5/Assets/FC_Scripts/CheatTP.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatTP : MonoBehaviour
+{
+    public GameObject player;
+
+    public Transform[] tpZones = new Transform[0];
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void TP(Transform position)
+    {
+        player.transform.Translate(position.position);
+    }
+}
