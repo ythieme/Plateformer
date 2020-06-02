a8c7f2126f1251a004f1a7adee154e5e1f2774ff
Félix <f.courtemanche@rubika-edu.com>
2020-06-02 22:09:34 +0200

    Anim réparée, Hugger intégré, platformes mouvantes moins glissantes

---
+++ Plateformer_equipe_5/Assets/FC_Scripts/Ennemies/Hugger/FlipX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipX : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public MovingPlatformScript_FC movingPlatform;
+
+    void Update()
+    {
+        SpriteFlip();
+    }
+
+    void SpriteFlip()
+    {
+        if (movingPlatform.actualSpeedX > 0 && spriteRenderer.flipX == false)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (movingPlatform.actualSpeedX < 0 && spriteRenderer.flipX == true)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else { }
+    }
+}
