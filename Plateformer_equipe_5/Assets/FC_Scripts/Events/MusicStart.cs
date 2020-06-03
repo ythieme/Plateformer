a587b89e40d2a54cd5788d361295ff5c00dde24e
Félix <f.courtemanche@rubika-edu.com>
2020-06-03 12:15:42 +0200

    Ajout préfab musique de poursuite

---
+++ Plateformer_equipe_5/Assets/FC_Scripts/Events/MusicStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicStart : MonoBehaviour
+{
+    public AudioSource epicness;
+
+    public bool isEnd;
+    public bool isPlaying;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player") && !isEnd && !isPlaying)
+        {
+            isPlaying = true;
+            epicness.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && isEnd)
+        {
+            epicness.Stop();
+        }
+    }
+}
